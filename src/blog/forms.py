d40f2ad387f99feec6152e1644c2bb3fe86e2cf1
AmarKakde <amarkakde21@gmail.com>
2024-08-03 21:51:22 +0530

    update post

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -4,10 +4,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ('title', 'title_tag', 'author', 'body')
     widgets = {
         'title': forms.TextInput(attrs={'class':'form-control'}),
         'title_tag': forms.TextInput(attrs={'class':'form-control'}),
-        'author': forms.TextInput(attrs={'class':'form-control'}),
-        'body': forms.TextInput(attrs={'class':'form-control'}),
+        'author': forms.Select(attrs={'class':'form-control'}),
+        'body': forms.Textarea(attrs={'class':'form-control'}),
     }
