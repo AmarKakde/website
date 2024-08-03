73c4b79c459b0e6c574a429b2ed5f6b1ea087398
AmarKakde <amarkakde21@gmail.com>
2024-08-03 22:09:53 +0530

    delete post

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import (ListView, DetailView, 
+                                  CreateView, UpdateView, DeleteView)
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
+
 
 # Create your views here.
 # def home(request):
@@ -10,13 +13,14 @@ from .forms import PostForm
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
 
 class AddPostView(CreateView):
-    model = Post
+    # model = Post
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
@@ -25,3 +29,8 @@ class UpdatePostView(UpdateView):
     model = Post
     fields = ['title', 'title_tag', 'body']
     template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
